7853c461a79b022e920bd1dc1addc9a33d164ab5
Alexandr Siryj <theobroma222@gmail.com>
2025-08-24 14:29:12 +0300

    format

--- type-safe-router/src/main.tsx
+++ type-safe-router/src/main.tsx
@@ -1,20 +1,22 @@
 import { StrictMode } from 'react';
+
 import './index.css';
-import App from './App.tsx';
 
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
 
+import App from './App.tsx';
+
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: (
-            <StrictMode>
-                <App />
-            </StrictMode>
-        ),
-    },
+  {
+    path: '/',
+    element: (
+      <StrictMode>
+        <App />
+      </StrictMode>
+    ),
+  },
 ]);
 
 const root = document.getElementById('root')!;
