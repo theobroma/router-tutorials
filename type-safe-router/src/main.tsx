f3e65df90349e4f5d7d046017c0fd0621489d6fe
Alexandr Siryj <theobroma222@gmail.com>
2025-08-28 23:13:38 +0300

    two routes

--- type-safe-router/src/main.tsx
+++ type-safe-router/src/main.tsx
@@ -5,20 +5,20 @@ import './index.css';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
+import { HomePage } from './pages/home/home.page';
+import { AboutPage } from './pages/about/about.page';
 
-import App from './App.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <StrictMode>
-        <App />
-      </StrictMode>
-    ),
+      children: [
+      { index: true, Component: HomePage },
+      { path: "about", Component: AboutPage },
+    ],
   },
 ]);
 
 const root = document.getElementById('root')!;
 
-ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(root).render(<StrictMode><RouterProvider router={router} /></StrictMode>);
